2ff261866ee3b0ea00f261ffdc3fc93e6333cc4c
HUNOR MORICZI <hm586@inlumine.ual.es>
2022-04-27 12:54:36 +0200

    Eliminacion de Code smells

--- Individuo.py
+++ Individuo.py
@@ -1,12 +1,10 @@
 from array import array
 import secrets
 class individuo:
-#atributos de cada individuo de la poblacion de la generacion actual
  
     #Declaro una lista de genes de tamaño 10 con valores entre 0 y 1
     genes = []
    
-
     #bucle para rellenar la lista de genes con valores 0 o 1
     # Se hace uso de secret() para generar un numero aleatorio entre 0 y 1 
     # ya que segun sonarcloud no se puede usar random.randint por seguridad
@@ -72,22 +70,3 @@ class individuo:
         #Creo un nuevo individuo con los genes de los padres cruzados
         individuoMutacion = individuo(genesMutacion)
         return individuoMutacion
-    
-
-#creo un individuo con los genes aleatorios generados y lo imprimo
-# individuo1 = individuo([])
-# individuo1.rellenarGenes()
-# print(individuo1.genes)
-
-# individuo2 = individuo([])
-# individuo2.rellenarGenes()
-# print(individuo2.genes)
-
-# puntoCruce = secrets.randbelow(10)
-# individuoCruzado2 = individuo1.cruzarPunto(individuo2, puntoCruce)
-# print(individuoCruzado2.genes)
-
-# individuoMutado = individuo1.mutar(0.2)
-
-# print(individuoMutado.genes)
-
